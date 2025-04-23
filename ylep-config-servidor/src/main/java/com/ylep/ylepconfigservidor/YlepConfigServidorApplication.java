e05c17423bbfde3faf508e770286ef18cb51c7cb
layder93122 <138517444+layder93122@users.noreply.github.com>
2025-04-23 08:40:43 -0500

    Add files via upload

---
+++ ylep-config-servidor/src/main/java/com/ylep/ylepconfigservidor/YlepConfigServidorApplication.java
@@ -0,0 +1,15 @@
+package com.ylep.ylepconfigservidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class YlepConfigServidorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YlepConfigServidorApplication.class, args);
+    }
+
+}
---
+++ ylep-config-servidor/src/test/java/com/ylep/ylepconfigservidor/YlepConfigServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ylep.ylepconfigservidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YlepConfigServidorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
