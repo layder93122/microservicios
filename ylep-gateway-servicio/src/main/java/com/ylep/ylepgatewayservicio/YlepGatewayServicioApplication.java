d58b270269cf1ee3937c576266847f0e63666a53
layder93122 <138517444+layder93122@users.noreply.github.com>
2025-04-23 09:09:09 -0500

    Add files via upload

---
+++ ylep-gateway-servicio/src/main/java/com/ylep/ylepgatewayservicio/YlepGatewayServicioApplication.java
@@ -0,0 +1,13 @@
+package com.ylep.ylepgatewayservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YlepGatewayServicioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YlepGatewayServicioApplication.class, args);
+    }
+
+}
---
+++ ylep-gateway-servicio/src/test/java/com/ylep/ylepgatewayservicio/YlepGatewayServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ylep.ylepgatewayservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YlepGatewayServicioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
